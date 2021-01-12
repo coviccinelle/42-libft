48d9efa17e4a173d5f2ea44e897c82d736367359
thi-phng <thi-phng@student.42.fr>
2021-01-12 09:08:26 +0100

    add str.mapi.c

--- stillworrkingon/ft_mapi.c
+++ stillworrkingon/ft_mapi.c
@@ -6,7 +6,7 @@
 /*   By: thi-phng <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/11 08:55:53 by thi-phng          #+#    #+#             */
-/*   Updated: 2021/01/11 08:56:03 by thi-phng         ###   ########.fr       */
+/*   Updated: 2021/01/12 08:59:06 by thi-phng         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,20 @@
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	jsp
+	char	*dst;
+	int		i;
+
+	i = 0;
+	if (s == NULL || f == NULL)
+		return (NULL);
+	dst = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (dst == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		dst[i] = f(i, s[i]);
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
 }
