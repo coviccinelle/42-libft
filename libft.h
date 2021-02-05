2f1e1702128fa97da3aaf2e3198b0a44e79fbf68
thi-phng <thi-phng@student.42.fr>
2021-02-05 14:07:41 +0100

    Passed 100% by Moulinette, rm restricts

--- libft.h
+++ libft.h
@@ -24,10 +24,8 @@ typedef struct	s_list
 }				t_list;
 
 size_t			ft_strlen(char const *s);
-size_t			ft_strlcat(char *restrict dst, const char *restrict src,
-							size_t dstsize);
-size_t			ft_strlcpy(char *restrict dst, const char *restrict src,
-							size_t dstsize);
+size_t			ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t			ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 char			*ft_strrchr(const char *str, int c);
 char			*ft_strtrim(char const *s1, char const *set);
@@ -56,12 +54,10 @@ int				ft_isprint(int c);
 int				ft_memcmp(const void *s1, const void *s2, size_t n);
 int				ft_lstsize(t_list *lst);
 
-void			*ft_memcpy(void *restrict dst, const void *restrict src,
-							size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			ft_bzero(void *s, size_t n);
 void			*ft_calloc(size_t count, size_t size);
-void			*ft_memccpy(void *restrict dest, const void *restrict src,
-						int c, size_t n);
+void			*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void			*ft_memchr(const void *s, int c, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memset(void *b, int c, size_t len);
